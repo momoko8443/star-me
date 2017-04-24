fd3fe7a351cd184e5ebe05f4e638c940092810de
Jake Zheng <momoko8443@163.com>
2017-04-24 20:53:48 +0800

    update code

--- star-me-api/routers/api.js
+++ star-me-api/routers/api.js
@@ -7,14 +7,22 @@ module.exports = function (server, config) {
     var router = express.Router();
     var userGithub = new UserGithub(config);
     router.get('/authenticatedUser', function (req, res) {
-        var token = cache.get(req.session.id);
-        if (token) {
-            userGithub.getAuthenticatedUser(token).then(function (user) {
-                res.send(user);
-            });
-        } else {
-            res.end();
+        if(req.session.user){
+            res.send(req.session.user);
+        }else{
+            var token = cache.get(req.session.id);
+            if (token) {
+                userGithub.getAuthenticatedUser(token).then(function (user) {
+                    req.session.user = user;
+                    req.session.save(function(err){
+                        res.send(user);
+                    }); 
+                });
+            } else {
+                res.end();
+            }
         }
+        
     });
 
     server.use('/api', router);

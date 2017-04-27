a0f0cf5d9c66755c2b191e1a9c1138537777939a
Jake Zheng <momoko8443@163.com>
2017-04-27 22:43:27 +0800

    store recommend data to issues

---
+++ star-me-api/services/github/IssueGithub.js
@@ -0,0 +1,110 @@
+var request = require('request');
+
+function IssueGithub(config) {
+    var api = config.api;
+    var app = config.app;
+
+    this.create = function (token, title, body) {
+        return new Promise(function (resolve, reject) {
+            if (token) {
+                var headers = { 'User-Agent': app.app_name, Authorization: 'token ' + token };
+                request({
+                    url: api.issue,
+                    json: true,
+                    method: 'POST',
+                    headers: headers,
+                    proxy: 'http://web-proxy.jpn.hp.com:8080',
+                    body: {
+                        title: title,
+                        body: body,
+                        label: ['wontfix']
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(body);
+                    }
+                });
+            } else {
+                reject();
+            }
+        });
+    };
+
+    this.addComment = function (token, issue_id, body) {
+        return new Promise(function (resolve, reject) {
+            if (token && issue_id) {
+                var url = api.comment.replace('{0}', issue_id);
+                console.log(url);
+                var headers = { 'User-Agent': app.app_name, Authorization: 'token ' + token };
+                request({
+                    url: url,
+                    json: true,
+                    method: 'POST',
+                    headers: headers,
+                    proxy: 'http://web-proxy.jpn.hp.com:8080',
+                    body: {
+                        body: body
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(body);
+                    }
+                });
+            } else {
+                reject();
+            }
+        });
+    };
+
+    this.findOne = function (token, repoName) {
+        return new Promise(function (resolve, reject) {
+            var url = api.search + '/issues?q=' + repoName + '+in:title+repo:momoko8443/star-me+type:issue&type=Issues';
+            console.log(url);
+            var headers = { 'User-Agent': app.app_name };
+            if (token) {
+                headers = { 'User-Agent': app.app_name, Authorization: 'token ' + token };
+            }
+            request({
+                url: url,
+                json: true,
+                headers: headers,
+                proxy: 'http://web-proxy.jpn.hp.com:8080'
+            }, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+    };
+
+    this.findAll = function (token) {
+        return new Promise(function (resolve, reject) {
+            var url = api.search + '/issues?q=recommend+in:title+repo:momoko8443/star-me+type:issue&type=Issues';
+            var headers = { 'User-Agent': app.app_name };
+            if (token) {
+                headers = { 'User-Agent': app.app_name, Authorization: 'token ' + token };
+            }
+            request({
+                url: url,
+                json: true,
+                headers: headers,
+                proxy: 'http://web-proxy.jpn.hp.com:8080'
+            }, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+
+        });
+    };
+}
+
+module.exports = IssueGithub;
--- star-me-app/app.js
+++ star-me-app/app.js
@@ -3,7 +3,7 @@ var session = require('express-session');
 var path = require('path');
 var config = require('./conf/config');
 var MongoStore = require('connect-mongo')(session);
-
+var bodyParser = require('body-parser');
 var server = require('star-me-api');
 
 var app = express();
@@ -15,13 +15,18 @@ app.use(session({
   store: new MongoStore(
     {
       url: 'mongodb://localhost/session',
-      autoRemove: 'interval',
-      autoRemoveInterval: 10
+      ttl: 1 * 1 * 5 * 60 // = 14 days. Default
+      // autoRemove: 'interval',
+      // autoRemoveInterval: 10
     })
 }));
 
 app.use(express.static(path.join(__dirname, 'node_modules/star-me-ui')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 
 server.auth(app, config);
 server.api(app, config);

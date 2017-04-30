e662f98d53183ffc811b1f1d535ab155439830da
Jake Zheng <momoko8443@163.com>
2017-04-30 21:25:56 +0800

    change mongo-session to cookie-session

--- star-me-app/app.js
+++ star-me-app/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var session = require('express-session');
 var path = require('path');
 var config = require('./conf/config');
-var MongoStore = require('connect-mongo')(session);
 var bodyParser = require('body-parser');
 var server = require('star-me-api');
 
@@ -12,21 +11,23 @@ app.use(session({
   secret: 'starme',
   saveUninitialized: true,
   resave: false,
-  store: new MongoStore(
-    {
-      url: 'mongodb://localhost/session',
-      ttl: 1 * 1 * 5 * 60 // = 14 days. Default
-      // autoRemove: 'interval',
-      // autoRemoveInterval: 10
-    })
+  cookie:{
+    maxAge: 1800000,
+    secure: false
+  }
 }));
 
 app.use(express.static(path.join(__dirname, 'node_modules/star-me-ui')));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(function(res,req,next){
+  console.log(res.sessionID);
+  next();
+})
 
 server.auth(app, config);
 server.api(app, config);
